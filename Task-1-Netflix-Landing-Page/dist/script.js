4325690fec8d8614cc0cb677c4222c24eb84a0f1
YASMEEN NAAZ <118757913+y-naaz@users.noreply.github.com>
2024-08-04 12:54:47 +0530

    Add files via upload

---
+++ Task-1-Netflix-Landing-Page/dist/script.js
@@ -0,0 +1,38 @@
+function mobileNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// Script for To Top button
+let toTopBtn = document.getElementById("to-top");
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    toTopBtn.style.display = "block";
+  } else {
+    toTopBtn.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome
+}
+
+// Script for responsive carousel
+const slider = document.querySelector(".slider");
+
+function activate(e) {
+  const items = document.querySelectorAll(".item");
+  e.target.matches(".next") && slider.append(items[0]);
+  e.target.matches(".prev") && slider.prepend(items[items.length - 1]);
+}
+
+document.addEventListener("click", activate, false);
